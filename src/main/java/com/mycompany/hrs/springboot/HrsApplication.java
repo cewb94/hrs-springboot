5f963ad860fbf322b49cbfd81492d1e5b2b59e40
unknown <yaqoubalshatti94@gmail.com>
2025-06-01 00:31:21 +0300

    Add existing project files prior to the push to GitHub.

---
+++ src/main/java/com/mycompany/hrs/springboot/HrsApplication.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hrs.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ *
+ * @author Yaqoub Alshatti
+ */
+@SpringBootApplication
+@EnableCaching        // Enable @Cacheable, @CacheEvict, etc.
+@EnableAsync          // Enable @Async on service methods
+public class HrsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HrsApplication.class, args);
+    }
+}
